0016a8554d182edcf6ec42bb77a950114b9fca40
vishal <vishal.saini.99154@gmail.com>
2024-07-23 10:38:53 +0530

    search page

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -1,6 +1,11 @@
 "use client";
 import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
+import Link from "next/link";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import PropertyCard from "@/components/PropertyCard";
+import Spiner from "@/components/Spiner";
+import PropertySearchForm from "@/components/PropertySearchForm";
 
 const SearchResultPage = () => {
   const searchParams = useSearchParams();
@@ -34,7 +39,40 @@ const SearchResultPage = () => {
   }, [loading, propertyType]);
 
   console.log(properties);
-  return <div>SearchResultPage</div>;
+  return (
+    <>
+      <section className="bg-blue-700 py-4 border-t">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+          <PropertySearchForm />
+        </div>
+      </section>
+      {loading ? (
+        <Spiner loading={loading} />
+      ) : (
+        <section className="px-4 py-6">
+          <div className="container-xl lg:container m-auto px-4 py-6">
+            <h1 className="text-2xl mb-4">Search Results</h1>
+            <Link
+              href="/properties"
+              className="flex items-center text-blue-500 mb-3 hover:underline"
+            >
+              <FaArrowAltCircleLeft className="mr-2 text-xl" />
+              Back to properties
+            </Link>
+            {properties.length === 0 ? (
+              <center> Oops! No Search Results Found!</center>
+            ) : (
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {properties.map((property) => (
+                  <PropertyCard key={property._id} property={property} />
+                ))}
+              </div>
+            )}
+          </div>
+        </section>
+      )}
+    </>
+  );
 };
 
 export default SearchResultPage;
