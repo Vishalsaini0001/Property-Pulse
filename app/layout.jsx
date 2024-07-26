dfecae3453704ad2a234ac328aa66bf7987375da
vishal <vishal.saini.99154@gmail.com>
2024-07-26 14:22:33 +0530

    dummy data files deleted

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,6 +11,7 @@ export const metadata = {
   title: "PropertyPulse | Find The Perfect Rental",
   description: "Find your dream rental property",
   keywords: "rental, find rentals, find properties",
+  
 };
 
 const MainLayout = ({ children }) => {
